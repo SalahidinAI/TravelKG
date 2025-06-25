de471fb414ab3ad5fca470063f0d33e1a0ba0323
SalahidinAI <zulpukarovz555@gmail.com>
2025-06-25 21:48:02 +0600

    sss

--- s.py
+++ s.py
@@ -1,39 +1,7 @@
-def isMatch(s, p):
-    if p.isalpha():
-        return s == p
-    new_p = p.replace('.*', '0')
-    len_p = len(new_p)
-    s_idx = -1
-    idx = 0
-    while s_idx < len(s) - 1:
-        s_idx += 1
-        i = s[s_idx]
-        # print(idx + 1 < len_p, '1111')
-        # print(new_p[idx] == i or new_p[idx] == '.')
-        if idx + 1 < len_p:
-            if new_p[idx + 1] == '*':
-                if new_p[idx] == i:
-                    continue
-                idx += 2
-                s_idx -= 1
-                continue
-
-        if len_p == idx:
-            return False
-
-        if new_p[idx] == '0':
-            if idx == len_p - 1:
-                break
-
-        # print(new_p[idx] != '.', new_p[idx] != i, '2222')
-        if new_p[idx] != '.' and new_p[idx] != i:
-            return False
-
-        idx += 1
-
-    return True
+# def isMatch(s, p):
 
 
-# print(isMatch("aab", "c*a*b"))
-# print(isMatch('mississippi', "mis*is*p*."))
-print(isMatch('a', "ab*a"))
+# print(isMatch('aa', 'a'))
+
+print('aa')
+print('aa')
