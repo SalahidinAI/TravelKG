270ee4af625d32d7b821af55d14c42fdb256db36
KadyrovErjan <kadyroverjan007@gmail.com>
2025-06-29 14:52:56 +0600

    CORRECT

--- mysite/travel/urls.py
+++ mysite/travel/urls.py
@@ -53,9 +53,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='login'),
 
     path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-
-]
-
-urlpatterns += [
     path('password_reset/verify_code/', verify_reset_code, name='verify_reset_code'),
+
 ]
