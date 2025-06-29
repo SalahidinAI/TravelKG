8e9ae48c828b356b73e8f0fe2782f41a1003df3d
KadyrovErjan <kadyroverjan007@gmail.com>
2025-06-29 14:49:21 +0600

    Erzhan True code, fixed CORRECT

--- mysite/travel/views.py
+++ mysite/travel/views.py
@@ -16,6 +16,17 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import VerifyResetCodeSerializer
+from rest_framework import generics, viewsets
+from .models import *
+from .serializers import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .coordinates import locations
+from geopy.distance import geodesic
+from drf_yasg.utils import swagger_auto_schema
+
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
@@ -208,18 +219,99 @@ class HomeCultureAPIView(generics.ListAPIView):
         return Culture.objects.filter(culture_variety__culture_variety_name='Home page')
 
 
-# @api_view(['POST'])
-# def toggle_book_like(request, book_id):
-#     try:
-#         book = Book.objects.get(id=book_id)
-#     except Book.DoesNotExist:
-#         return Response({'detail': 'Book not found'}, status=status.HTTP_404_NOT_FOUND)
-#
-#     like, created = Like.objects.get_or_create(user=request.user, book=book)
-#
-#     if not created:
-#         like.delete()
-#         return Response({'detail': 'Like deleted'}, status=status.HTTP_404_NOT_FOUND)
-#
-#     return Response({'detail': 'Like created'}, status=status.HTTP_201_CREATED)
+@api_view(['POST'])
+def toggle_review_place_like(request, review_place_id):
+    try:
+        review_place = ReviewPlace.objects.get(id=review_place_id)
+    except ReviewPlace.DoesNotExist:
+        return Response({'detail': 'Place review not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    like, created = ReviewPlaceLike.objects.get_or_create(user=request.user, review_place=review_place)
+
+    if not created:
+        like.delete()
+        return Response({'detail': 'Like is deleted'}, status=status.HTTP_404_NOT_FOUND)
+
+    return Response({'detail': 'Like is created'}, status=status.HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+def toggle_review_hotel_like(request, review_hotel_id):
+    try:
+        review_hotel = ReviewHotel.objects.get(id=review_hotel_id)
+    except ReviewHotel.DoesNotExist:
+        return Response({'detail': 'Hotel review not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    like, created = ReviewHotelLike.objects.get_or_create(user=request.user, review_hotel=review_hotel)
+
+    if not created:
+        like.delete()
+        return Response({'detail': 'Like is deleted'}, status=status.HTTP_404_NOT_FOUND)
+
+    return Response({'detail': 'Like is created'}, status=status.HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+def toggle_review_restaurant_like(request, review_restaurant_id):
+    try:
+        restaurant = ReviewRestaurant.objects.get(id=review_restaurant_id)
+    except ReviewRestaurant.DoesNotExist:
+        return Response({'detail': 'Restaurant review not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    like, created = ReviewRestaurantLike.objects.get_or_create(user=request.user, restaurant=restaurant)
+
+    if not created:
+        like.delete()
+        return Response({'detail': 'Like is deleted'}, status=status.HTTP_404_NOT_FOUND)
+
+    return Response({'detail': 'Like is created'}, status=status.HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+def toggle_review_attraction_like(request, review_attraction_id):
+    try:
+        attraction = ReviewAttraction.objects.get(id=review_attraction_id)
+    except ReviewAttraction.DoesNotExist:
+        return Response({'detail': 'Attraction review not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    like, created = ReviewAttractionLike.objects.get_or_create(user=request.user, attraction=attraction)
+
+    if not created:
+        like.delete()
+        return Response({'detail': 'Like is deleted'}, status=status.HTTP_404_NOT_FOUND)
+
+    return Response({'detail': 'Like is created'}, status=status.HTTP_201_CREATED)
+
+
+# travel/views.py
+
+class TravelDistanceAPIView(APIView):
+    @swagger_auto_schema(request_body=TravelRequestSerializer)  # 👈 ЭТО ВАЖНО
+    def post(self, request):
+        serializer = TravelRequestSerializer(data=request.data)
+        if serializer.is_valid():
+            from_city = serializer.validated_data['from_city']
+            to_city = serializer.validated_data['to_city']
+
+            coord_from = locations.get(from_city)
+            coord_to = locations.get(to_city)
+
+            if not coord_from or not coord_to:
+                return Response({"error": "Unknown city name"}, status=status.HTTP_400_BAD_REQUEST)
+
+            distance = geodesic(coord_from, coord_to).km
+
+            return Response({
+                "from": from_city,
+                "to": to_city,
+                "distance_km": round(distance, 2),
+                "time": {
+                    "walking_hours": round(distance / 5, 2),
+                    "driving_hours": round(distance / 70, 2),
+                    "train_hours": round(distance / 80, 2),
+                    "plane_hours": round(distance / 600, 2)
+                }
+            })
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
