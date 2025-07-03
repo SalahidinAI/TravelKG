d66b965dac480e66184c1e7aa7dd087667d240ae
SalahidinAI <zulpukarovz555@gmail.com>
2025-07-03 11:58:28 +0600

    After Merge, code from Salah

--- mysite/travel/urls.py
+++ mysite/travel/urls.py
@@ -63,3 +63,4 @@ urlpatterns = [
     path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
     path('password_reset/verify_code/', verify_reset_code, name='verify_reset_code'),
 ]
+
